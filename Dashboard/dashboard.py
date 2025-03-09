66d96b07ced6a94b0d4d32edfa09b3a9113c2ab3
Christian <christiannathanielp@gmail.com>
2025-03-09 15:47:25 +0700

    try again

--- Dashboard/dashboard.py
+++ Dashboard/dashboard.py
@@ -6,7 +6,8 @@ import seaborn as sns
 
 # Load Dataset
 def load_data():
-    df = pd.read_csv("https://raw.githubusercontent.com/Data_Analysis_Project-Ecommerce/main/Dashboard/cleaned_ecommerce_data.csv")  
+    df = pd.read_csv("cleaned_ecommerce_data.csv")  
+    # df = pd.read_csv("https://raw.githubusercontent.com/Data_Analysis_Project-Ecommerce/main/Dashboard/cleaned_ecommerce_data.csv")  
     return df
 
 df = load_data()
